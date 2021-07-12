7a1e4012e962872ea0d86a01e6d2184d88c38cd5
CherchiiK <igordudka2033@gmail.com>
2021-07-12 13:28:29 +0300

    Add final part of homework

--- src/part2/StringSort.java
+++ src/part2/StringSort.java
@@ -12,23 +12,42 @@ public class StringSort {
                 int c = (chars.length/2) + 1;
                 b = a.substring(v, c);
             }
+            else if (chars.length % 2 != 0){
+                int v = (chars.length/2);
+                int c = (chars.length/2) + 1;
+                b = o.substring(v,c);
+            }
         return b;
     }
 
-    String middleStringElemInDivisible(String o){
-        char [] chars = o.toCharArray();
-        String q = null;
-        if (chars.length % 2 != 0){
+    String arrayReplacing(String a){
+        char [] chars = a.toCharArray();
+        String k,m,z,w;
+        String b = null;
+        if (chars.length % 2 ==0){
             int v = (chars.length/2);
-            int c = (chars.length/2) + 1;
-            q = o.substring(v,c);
+            int c = (chars.length/2);
+            k = a.substring(0, c);
+            m = a.substring(v, chars.length);
+            b = m + k;
         }
-        return q;
+        else if (chars.length % 2 != 0){
+            int y = (chars.length/2);
+            z = o.substring(0,y);
+            m = o.substring(y, y+1);
+            w = o.substring(y+1, chars.length);
+            b = w+ m + z;
+        }
+        return b;
     }
+
+
 
     public static void main(String[] args) {
         StringSort stringSort = new StringSort();
         System.out.println(stringSort.middleStringElemDivisible(stringSort.a));
-        System.out.println(stringSort.middleStringElemInDivisible(stringSort.o));
+        System.out.println(stringSort.middleStringElemDivisible(stringSort.o));
+        System.out.println(stringSort.arrayReplacing(stringSort.a));
+        System.out.println(stringSort.arrayReplacing(stringSort.o));
     }
 }
