11ea351cbfcffffa5bdda8566d662407a8b6ea85
CherchiiK <igordudka2033@gmail.com>
2021-07-12 10:25:03 +0300

    Added method plus which returns sum of elements in array

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,15 @@
 public class Calculator {
+    private int [] myArray = new int[4];
     int plus(int a,int b){
         return a + b;
+    }
+
+    int plus(int [] myArray) {
+        int sum = 0;
+        for (int j : myArray) {
+            sum = sum + j;
+        }
+        System.out.println(sum);
+        return sum;
     }
 }
