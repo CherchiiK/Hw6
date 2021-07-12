1a77c4fcee901a774456cc71b73b14cb991ff72a
CherchiiK <igordudka2033@gmail.com>
2021-07-12 12:47:04 +0300

    Fixed some things

--- src/part2/StringSort.java
+++ src/part2/StringSort.java
@@ -5,24 +5,22 @@ public class StringSort {
     private String a = "abcdef";
 
     String middleStringElemDivisible(String a){
-        int stringLength = a.length();
         char [] chars = a.toCharArray();
         String b = null;
             if (chars.length % 2 ==0){
-                int v = (stringLength/2) - 1;
-                int c = (stringLength/2) + 1;
+                int v = (chars.length/2) - 1;
+                int c = (chars.length/2) + 1;
                 b = a.substring(v, c);
             }
         return b;
     }
 
     String middleStringElemInDivisible(String o){
-        int stringLength = o.length();
         char [] chars = o.toCharArray();
         String q = null;
         if (chars.length % 2 != 0){
-            int v = (stringLength/2);
-            int c = (stringLength/2) + 1;
+            int v = (chars.length/2);
+            int c = (chars.length/2) + 1;
             q = o.substring(v,c);
         }
         return q;
